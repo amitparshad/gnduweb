04bde951d15402165cddc6d59fbdca1f0762d3f9
Sanyam <sanyamjhingan99@gmail.com>
2019-03-05 15:27:38 +0530

    fixing amit's mistakes

--- accounts/forms.py
+++ accounts/forms.py
@@ -35,12 +35,12 @@ class studentreg(forms.ModelForm):
 
 class userform(forms.ModelForm):
 
-    first_name = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'First Name '}),required = True,max_length=30)
+    first_name = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'First Name'}),required = True,max_length=30)
     last_name = forms.CharField(widget = forms.TextInput(attrs={'placeholder':'Last Name'}),max_length = 30)
     email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder':'Email'}),required=True,max_length=50)
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}),required = True,max_length= 50)
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password'}),required=True,max_length=50)
 
-    class Meta():
+    class Meta:
         model = User
-        fields = ['first_name','last_name','email','password']
+        fields = ('first_name','last_name','email','password')
